fc83c3d5e42732ab6d96a6168dc05ac73506647b
Arturo Ortega <macarthur.or@gmail.com>
2019-07-22 12:47:03 -0500

    Multi Language support and documentation

--- index.js
+++ index.js
@@ -4,40 +4,41 @@ const path = require('path')
 const nodemailer = require("nodemailer")
 
 /**
- * Summary. (use period)
- *
- * Description. (use period)
- *
- * @since      1.0.0
- * @access     private
+ * Main function to send emails.
  *
  * @alias    SmtpMailAdapter
  *
  * Required
- * @param {Object}   mailOptions    Parameters from the parse declaration  
- * @param {Object}   mailOptions.host    Parameters from the parse declaration  
- * @param {Object}   mailOptions.port    Parameters from the parse declaration  
- * @param {Object}   mailOptions.user    Parameters from the parse declaration  
- * @param {Object}   mailOptions.password    Parameters from the parse declaration  
- * @param {Object}   mailOptions.fromAddress    Parameters from the parse declaration  
+ * @param {Object}   mailOptions                Parameters from the parse declaration.
+ * @param {Object}   mailOptions.host           used to create the Transport frunction of nodemailer.
+ * @param {Object}   mailOptions.port           used to create the Transport frunction of nodemailer.
+ * @param {Object}   mailOptions.user           used to create the Transport frunction of nodemailer.
+ * @param {Object}   mailOptions.password       used to create the Transport frunction of nodemailer.
+ * @param {Object}   mailOptions.fromAddress    used to create the Transport frunction of nodemailer.
  * Optionals
  * /- nodemailer Option -/
- * @param {boolean}   [secure=false]  Description of optional variable with default variable.
- * /- SmtpMailAdapter Options -/
- * @param {boolean}   [template=false]  Description of optional variable with default variable.
- * @param {String}    [templatePath=""] Description of optional variable with default variable.
- * @param {String}    [confirmSubject=mail.subject]  Description of optional variable with default variable.
- * @param {String}    [passwordSubject=mail.subject]  Description of optional variable with default variable.
- * @param {String}    [confirmbody="You are being asked to confirm the e-mail address"]  Description of optional variable with default variable.
- * @param {String}    [passwordbody="You requested to reset your password"]  Description of optional variable with default variable.
- * /- Multi Template Options -/
- * @param {boolean}   [multiTemplate]  Description of optional variable with default variable.
- * @param {String}    [confirmTemplatePath] Description of optional variable with default variable.
- * @param {String}    [passwordTemplatePath] Description of optional variable with default variable.
+ * @param {boolean}   [secure=false]            Deside if you connect with your SMTP server with a secure connection.
+ * /- sendMail Options -/
+ * @param {boolean}   [template=false]          Define if use the default template or a custom one.
+ * @param {String}    [templatePath=""]         Path of the custom template.
  * 
- * @param {boolean}   [multiLang]  Description of optional variable with default variable.
- * @param {Object}    [pwdMultiSubject]  Description of optional variable with default variable.
- * @param {Object}    [confMultiSubject]  Description of optional variable with default variable.
+ * @param {Object}    passwordOptions           Object with all the minimum data to customize the password email.
+ * @param {Object}    passwordOptions.btn       Text of the action button.
+ * @param {Object}    passwordOptions.body      Text of the email body.
+ * @param {Object}    passwordOptions.subject   Subject of the password recovery email.
+
+ * @param {Object}    confirmOptions            Object with all the minimum data to customize the confirm email.
+ * @param {Object}    confirmOptions.btn        Text of the action button.
+ * @param {Object}    confirmOptions.body       Text of the email body.
+ * @param {Object}    confirmOptions.subject    Subject of the confirmation email.
+ * /- Multi Template Options -/
+ * @param {boolean}   [multiTemplate]           If is True you can use a different template for email confirmation and password recovery.
+ * @param {String}    [confirmTemplatePath]     Path of the template file.
+ * @param {String}    [passwordTemplatePath]    Path of the template file.
+ * 
+ * @param {boolean}   [multiLang]               When is True the emails can be send in different languages depending on the "lang" colum of the user object.
+ * @param {Object}    [multiLangPass]           Object with all the translations for the password recovery email.
+ * @param {Object}    [multiLangConfirm]        Object with all the translations for the confirmation email.
  *
  * @return {Object} returns one or three functions, depending if you are using multiTemplate
  */
@@ -46,10 +47,11 @@ var SmtpMailAdapter = mailOptions => {
         throw "SMTP mail adapter requires host, port, fromAddress, user and password"
     }
 
-    var _templates = mailOptions.template || false
-    var _templatePath = mailOptions.templatePath || ""
-
-    // 
+    var _templates = mailOptions.template || false;
+    var _templatePath = mailOptions.templatePath || "";
+    var _multiTemplate = mailOptions.multiTemplate || false;
+    var _multiLang = mailOptions.multiLang || false;
+ 
     var transport = nodemailer.createTransport({
         host: mailOptions.host,
         port: mailOptions.port,
@@ -63,8 +65,10 @@ var SmtpMailAdapter = mailOptions => {
     /**
      * Sends the emails with one template for both types (password recovery and email verification).
      *
-     * In the template you can use only 4 variables from Parse.
+     * In the template you can use only 6 variables from Parse.
      * - link
+     * - btn
+     * - body
      * - username
      * - appName
      * - subject
@@ -77,28 +81,33 @@ var SmtpMailAdapter = mailOptions => {
      * @see  sendMail/MailAdapter
      * @link node_modules\parse-server\lib\Adapters\Email\MailAdapter.js
      *
-     * @param {Object}  mail    From parse, it contains the minimum to send the mail
-     * @param {String}  mail.to    Recipient's email
-     * @param {String}  mail.text    Email body with the link and username
+     * @param {Object}  mail            From parse, it contains the minimum to send the mail
+     * @param {String}  mail.to         Recipient's email
+     * @param {String}  mail.text       Email body with the link and username
      * @param {String}  mail.subject    Email subject
      *
-     * @return {type} Return value description.
+     * @return {type}
      */
     var sendMail = mail => {
-        console.log("sendMail")
         let link = mail.text.split("it:\n")[1];
-        let username = mail.text.split("username=")[1];
         let appName = mail.subject.split("for ")[1];
+        let username = mail.text.split("username=")[1];
         var filePath = "";
         var template = "";
+        const confirmOptions = mailOptions.confirmOptions || {};
+        const passwordOptions = mailOptions.passwordOptions || {};
         
         let subject = mail.subject.indexOf("Password") !=-1 
-                        ? mailOptions.passwordSubject || mail.subject 
-                        : mailOptions.confirmSubject || mail.subject;
+                        ? passwordOptions.subject || mail.subject 
+                        : confirmOptions.subject || mail.subject;
 
         let body = mail.subject.indexOf("Password") !=-1 
-                        ? mailOptions.passwordbody || "You requested to reset your password" 
-                        : mailOptions.confirmbody || "You are being asked to confirm the e-mail address";
+                        ? passwordOptions.body || "You requested to reset your password" 
+                        : confirmOptions.body || "You are being asked to confirm the e-mail address";
+        
+        let btn = mail.subject.indexOf("Password") !=-1 
+                        ? passwordOptions.btn || "Reset Password" 
+                        : confirmOptions.btn || "Confirm Email";
 
         if (_templates) {
             filePath = path.join("./", _templatePath);
@@ -125,15 +134,157 @@ var SmtpMailAdapter = mailOptions => {
             });
     };
 
+    /**
+     * Summary. (use period)
+     *
+     * Description. (use period)
+     *
+     * @since      2.0.0
+     * @access     private
+     *
+     * @alias    sendVerificationEmail
+     * @memberof SmtpMailAdapter
+     *
+     * @see  sendMail/MailAdapter
+     * @link node_modules\parse-server\lib\Adapters\Email\MailAdapter.js
+     *
+     * @param {Object}     data            Data from Parse Server.
+     * @param {String}     data.link       Link to verify the email.
+     * @param {String}     data.appName    Name of your App.
+     * @param {ParseUser}  data.user       The object of the user you want to ferify.
+     *
+     * @param {Object}     mailOptions.confirmOptions           Manimum options to send the email
+     * @param {Object}     mailOptions.confirmOptions.btn       Text of the email action button.
+     * @param {Object}     mailOptions.confirmOptions.body      Text of the email budy
+     * @param {Object}     mailOptions.confirmOptions.subject   Email subject.
+     * 
+     * @param {String}     mailOptions.confirmTemplatePath      Path of the template file.
+
+     * @param {Boolean}    [_multiLang]                         If it's true you can send the emails in different languages (depending on the lang colum in the _User object).
+     * @param {Object}     [mailOptions.multiLangConfirm]       Object with all the translations.
+     * 
+     * @return
+     */
     var sendVerificationEmail = data => {
-        console.log("sendVerificationEmail")
+        if(!mailOptions.confirmTemplatePath || !mailOptions.confirmOptions) {
+            throw "You need to add a template for the confirmation emails and pass the options";
+        } else if(_multiLang && !mailOptions.multiLangConfirm) {
+            throw "To use multiLang in the templates needs to pass the multiLangPass object with the translations";
+        }
+
+        const user = data.user.attributes;
+        const link = data.link;
+        const appName = data.appName;
+        const options = mailOptions.confirmOptions;
+        const langOptions = mailOptions.multiLangConfirm[user.lang];
+
+        let subject = (_multiLang && typeof langOptions !== 'undefined')
+                        ? langOptions.subject
+                        : options.subject
+
+        let body = (_multiLang && typeof langOptions !== 'undefined')
+                        ? langOptions.body
+                        : options.body
+        
+        let btn = (_multiLang && typeof langOptions !== 'undefined')
+                        ? langOptions.btn
+                        : options.btn
+
+        let filePath = path.join("./", mailOptions.confirmTemplatePath);
+        let template = eval('`' + fs.readFileSync(filePath).toString() + '`');
+
+        var senderOptions = {
+            from: mailOptions.fromAddress,
+            to: user.email,
+            subject: subject,
+            html: template
+        };
+
+        return transport.sendMail(senderOptions)
+            .then(() => {
+                return
+            })
+            .catch(error => {
+                throw error
+            });
     };
 
+    /**
+     * Sends an email to recover the password with a template
+     *
+     * Description. (use period)
+     *
+     * @since      2.0.0
+     * @access     private
+     *
+     * @alias    sendPasswordResetEmail
+     * @memberof SmtpMailAdapter
+     *
+     * @see  sendMail/MailAdapter
+     * @link node_modules\parse-server\lib\Adapters\Email\MailAdapter.js
+     *
+     * @param {Object}     data            Data from Parse Server.
+     * @param {String}     data.link       Link to recover the password.
+     * @param {String}     data.appName    Name of your App.
+     * @param {ParseUser}  data.user       The object of the user you want to ferify.
+     * 
+     * @param {Object}     mailOptions.passwordOptions          Description of optional variable.
+     * @param {Object}     mailOptions.passwordOptions.btn      Text of the email action button.
+     * @param {Object}     mailOptions.passwordOptions.body     Text of the email budy
+     * @param {Object}     mailOptions.passwordOptions.subject  Email subject.
+     * 
+     * @param {String}     mailOptions.passwordTemplatePath     Path of the template file.
+
+     * @param {Boolean}    [_multiLang]                         If it's true you can send the emails in different languages (depending on the lang colum in the _User object).
+     * @param {Object}     [mailOptions.multiLangPass]          Object with all the translations.
+     *
+     * @return
+     */
     var sendPasswordResetEmail = data => {
-        console.log("sendPasswordResetEmail")
+        if(!mailOptions.passwordTemplatePath || !mailOptions.passwordOptions) {
+            throw "You need to add a template for the password recovery emails";
+        } else if(_multiLang && !mailOptions.multiLangPass) {
+            throw "To use multiLang in the templates needs to pass the multiLangPass object with the translations"; 
+        }
+        
+        const user = data.user.attributes;
+        const link = data.link;
+        const appName = data.appName;
+        const options = mailOptions.passwordOptions;
+        const langOptions = mailOptions.multiLangPass[user.lang];
+
+        let subject = (_multiLang && typeof langOptions !== 'undefined')
+                        ? langOptions.subject
+                        : options.subject
+
+        let body = (_multiLang && typeof langOptions !== 'undefined')
+                        ? langOptions.body
+                        : options.body
+        
+        let btn = (_multiLang && typeof langOptions !== 'undefined')
+                        ? langOptions.btn
+                        : options.btn
+
+        let filePath = path.join("./", mailOptions.passwordTemplatePath);
+        let template = eval('`' + fs.readFileSync(filePath).toString() + '`');
+        
+        var senderOptions = {
+            from: mailOptions.fromAddress,
+            to: user.email,
+            subject: subject,
+            html: template
+        };
+
+        return transport.sendMail(senderOptions)
+            .then(() => {
+                return
+            })
+            .catch(error => {
+                throw error
+            });
     };
 
-    return (mailOptions.multiTemplate !== true) 
+    return (_multiTemplate !== true) 
     ? Object.freeze({ sendMail: sendMail })
     : Object.freeze({
         sendMail: sendMail,
@@ -142,4 +293,4 @@ var SmtpMailAdapter = mailOptions => {
     });
 }
 
-module.exports = SmtpMailAdapter
+module.exports = SmtpMailAdapter;
