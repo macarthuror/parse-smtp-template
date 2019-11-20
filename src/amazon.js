ecfaf13e73154d64b35973a1c6e55420b90bccb3
Arturo Ortega <macarthur.or@gmail.com>
2019-11-19 19:16:07 -0600

    :sparkles: :art: :heavy_plus_sign: test next ver

---
+++ src/amazon.js
@@ -0,0 +1,8 @@
+/**
+ * @author Arturo Ortega - macarthuror
+ *
+ * Some of the variables have a error label of "is never read" but is because how are they use it in the template
+ * So don't worry about it
+ */
+
+// https://medium.com/@yashoda.charith10/sending-emails-using-aws-ses-nodejs-460b8cc6d0d5
